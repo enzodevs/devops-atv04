434a72f98a81474a86058e678bf26f89aea04b0c
Enzo Lopes Cambraia Baptista <223335@facens.br>
2025-09-02 20:24:11 -0300

    Initial Commit

---
+++ src/test/java/br/com/facens/atividade4/Atividade4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.facens.atividade4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
