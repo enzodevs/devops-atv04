9c5e072f894a3c140632da6189dac0c4f7c3187e
GustavoBotejara <gustavosintobotejara@gmail.com>
2025-09-02 21:03:38 -0300

    feat: created unit tests

--- src/test/java/br/com/facens/atividade4/Atividade4ApplicationTests.java
+++ src/test/java/br/com/facens/atividade4/Atividade4ApplicationTests.java
@@ -9,5 +9,34 @@ class Atividade4ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test 
+	public void deveConceder3CursosParaAlunoComMediaBoa() {
+		Aluno aluno = new Aluno("ASSINATURA_BASICA");
+		int cursosIniciais = aluno.getCursosDisponiveis();
+		ServicoDeCursos servico = new ServicoDeCursos();
+		servico.finalizarCurso(aluno, 8.0);
+		int cursosEsperados = cursosIniciais + 3;
+		assertEquals(cursosEsperados, aluno.getCursosDisponiveis());
+	}
+		
+	@Test 
+	public void naoDeveConcederCursosParaAlunoComMediaInsuficiente() {
+		Aluno aluno = new Aluno("ASSINATURA_BASICA");
+		int cursosIniciais = aluno.getCursosDisponiveis();
+		ServicoDeCursos servico = new ServicoDeCursos();
+		servico.finalizarCurso(aluno, 6.9);
+		int cursosEsperados = cursosIniciais + 3;
+		assertEquals(cursosEsperados, aluno.getCursosDisponiveis());
+	}
+	
+	@Test 
+	public void deveConcederCursosParaAlunoComMediaExatamenteNoLimite() {
+		Aluno aluno = new Aluno("ASSINATURA_BASICA");
+		int cursosIniciais = aluno.getCursosDisponiveis();
+		ServicoDeCursos servico = new ServicoDeCursos();
+		servico.finalizarCurso(aluno, 7.0);
+		int cursosEsperados = cursosIniciais + 3;
+		assertEquals(cursosEsperados, aluno.getCursosDisponiveis());
+	}
 }
